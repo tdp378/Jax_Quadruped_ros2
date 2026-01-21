55d4010ec0581d470b77b7b5821884e84e591c16
tdp378 <tdp378@yahoo.com>
2026-01-21 14:55:57 -0500

    Update __init__.py

--- jax_ws/src/jax_servo_interfacing/jax_servo_interfacing/hardware/__init__.py
+++ jax_ws/src/jax_servo_interfacing/jax_servo_interfacing/hardware/__init__.py
@@ -1,10 +1,1 @@
-from jax_servo_interfacing.joint_names
-import JOINT_NAME_MATRIX
 
-self.joint_name_to_index = {}
-
-for axis in range(3):
-  for leg in range(4):
-    joint_name = JOINT_NAME_MATRIX[axis][leg]
-
-self.joint_name_to_index[joint_name] = (axis, leg)
