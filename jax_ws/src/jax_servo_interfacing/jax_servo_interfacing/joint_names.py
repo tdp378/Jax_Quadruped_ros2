d407e1d7cc0d5cd898f69f437c99a2427aa515da
tdp378 <tdp378@yahoo.com>
2026-01-21 14:35:24 -0500

    Update joint_names.py

--- jax_ws/src/jax_servo_interfacing/jax_servo_interfacing/joint_names.py
+++ jax_ws/src/jax_servo_interfacing/jax_servo_interfacing/joint_names.py
@@ -1,1 +1,15 @@
+LEG_NAMES = [
+    "front_right",
+    "front_left",
+    "rear_right",
+    "rear_left",
+]
+
+JOINT_NAMES = ["hip", "thigh", "calf"]
+
+JOINT_NAMES_FULL = [
+    f"{leg}_{joint}_joint"
+    for leg in LEG_NAMES
+    for joint in JOINT_NAMES
+]
 
