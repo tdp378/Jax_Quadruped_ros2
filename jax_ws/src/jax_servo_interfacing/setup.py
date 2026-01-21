5662506d951835530f68faae2c850140cac31b72
tdp378 <tdp378@yahoo.com>
2026-01-21 14:17:45 -0500

    Update setup.py

--- jax_ws/src/jax_servo_interfacing/setup.py
+++ jax_ws/src/jax_servo_interfacing/setup.py
@@ -1,26 +1,26 @@
-from setuptools import setup, find_packages 
+from setuptools import setup, find_packages
 
 package_name = 'jax_servo_interfacing'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='Tim Powell',
-    maintainer_email='todo@todo.com',
-    description='Servo hardware interface for Jax quadruped',
-    license='MIT',
-    tests_require=['pytest'],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Tim Powell',
+    maintainer_email='you@example.com',
+    description='Servo interfacing for JAX quadruped',
+    license='MIT',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'hardware_interface = jax_servo_interfacing.hardware_interface:main',
+            'jax_servo_node = jax_servo_interfacing.jax_servo_node:main',
         ],
     },
 )
