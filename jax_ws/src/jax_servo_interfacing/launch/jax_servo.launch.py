1a3b98ed5e16ff774aba82d016d8ad4224a45c96
tdp378 <tdp378@yahoo.com>
2026-01-21 15:02:11 -0500

    Update jax_servo.launch.py

--- jax_ws/src/jax_servo_interfacing/launch/jax_servo.launch.py
+++ jax_ws/src/jax_servo_interfacing/launch/jax_servo.launch.py
@@ -1,1 +1,14 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='jax_servo_interfacing',
+            executable='jax_servo_node',
+            name='jax_servo_node',
+            output='screen'
+        )
+    ])
 
