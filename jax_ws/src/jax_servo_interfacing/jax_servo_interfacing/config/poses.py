f828741b41a86162e1cc8838dfebe3bf7eb419b6
tdp378 <tdp378@yahoo.com>
2026-01-23 11:20:47 -0500

    Update poses.py

--- jax_ws/src/jax_servo_interfacing/jax_servo_interfacing/config/poses.py
+++ jax_ws/src/jax_servo_interfacing/jax_servo_interfacing/config/poses.py
@@ -22,3 +22,47 @@ STAND_POSE = {
     "rear_right_knee_joint": 1.0,
 }
 
+SIT_POSE = { 
+    # Front Left
+    "front_left_hip_joint": 0.0,
+    "front_left_thigh_joint": -0.8,
+    "front_left_knee_joint": 1.6,
+
+    # Front Right
+    "front_right_hip_joint": 0.0,
+    "front_right_thigh_joint": -0.8,
+    "front_right_knee_joint": 1.6,
+
+    # Rear Left
+    "rear_left_hip_joint": 0.0,
+    "rear_left_thigh_joint": -1.0,
+    "rear_left_knee_joint": 1.8,
+
+    # Rear Right
+    "rear_right_hip_joint": 0.0,
+    "rear_right_thigh_joint": -1.0,
+    "rear_right_knee_joint": 1.8,
+}
+
+
+TUCK_POSE = {
+    # Front Left
+    "front_left_hip_joint": 0.2,
+    "front_left_thigh_joint": -1.2,
+    "front_left_knee_joint": 2.0,
+
+    # Front Right
+    "front_right_hip_joint": -0.2,
+    "front_right_thigh_joint": -1.2,
+    "front_right_knee_joint": 2.0,
+
+    # Rear Left
+    "rear_left_hip_joint": 0.2,
+    "rear_left_thigh_joint": -1.2,
+    "rear_left_knee_joint": 2.0,
+
+    # Rear Right
+    "rear_right_hip_joint": -0.2,
+    "rear_right_thigh_joint": -1.2,
+    "rear_right_knee_joint": 2.0,
+}
