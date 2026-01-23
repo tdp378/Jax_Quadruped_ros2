4bd6b0d05c11abc726f4f3d774edb2fea27bc1aa
tdp378 <tdp378@yahoo.com>
2026-01-23 11:17:12 -0500

    Update poses.py

--- jax_ws/src/jax_servo_interfacing/jax_servo_interfacing/config/poses.py
+++ jax_ws/src/jax_servo_interfacing/jax_servo_interfacing/config/poses.py
@@ -1,1 +1,24 @@
+# jax_servo_interfacing/config/poses.py 
+
+STAND_POSE = {
+    # Front Left
+    "front_left_hip_joint": 0.0,
+    "front_left_thigh_joint": -0.5,
+    "front_left_knee_joint": 1.0,
+
+    # Front Right
+    "front_right_hip_joint": 0.0,
+    "front_right_thigh_joint": -0.5,
+    "front_right_knee_joint": 1.0,
+
+    # Rear Left
+    "rear_left_hip_joint": 0.0,
+    "rear_left_thigh_joint": -0.5,
+    "rear_left_knee_joint": 1.0,
+
+    # Rear Right
+    "rear_right_hip_joint": 0.0,
+    "rear_right_thigh_joint": -0.5,
+    "rear_right_knee_joint": 1.0,
+}
 
