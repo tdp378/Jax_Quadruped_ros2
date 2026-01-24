deac0cec49b7ef64cd2d57717c4ad709956fe6a3
Tim <tdp378@yahoo.com>
2026-01-24 08:25:21 -0500

    fixed setup

--- jax_ws/install/jax_servo_interfacing/share/jax_servo_interfacing/launch/jax_servo.launch.py
+++ jax_ws/install/jax_servo_interfacing/share/jax_servo_interfacing/launch/jax_servo.launch.py
@@ -1,1 +1,14 @@
-/home/tdp378/Jax_Quadruped_ros2/jax_ws/build/jax_servo_interfacing/launch/jax_servo.launch.py
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='jax_servo_interfacing',
+            executable='jax_servo_node',
+            name='jax_servo_node',
+            output='screen'
+        )
+    ])
+
--- jax_ws/src/jax_description/setup.py
+++ jax_ws/src/jax_description/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'jax_description'
 
@@ -10,20 +12,26 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
+        # Install launch files
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*.py')),
+
+        # Install URDF / xacro
+        (os.path.join('share', package_name, 'urdf'),
+            glob('urdf/*')),
+
+        # Install RViz configs
+        (os.path.join('share', package_name, 'rviz'),
+            glob('rviz/*.rviz')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='tdp378',
     maintainer_email='tdp378@yahoo.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Jax quadruped robot description',
+    license='MIT',
     extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
-    entry_points={
-        'console_scripts': [
-        ],
+        'test': ['pytest'],
     },
 )
