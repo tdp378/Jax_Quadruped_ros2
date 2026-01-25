964a6dd4e15f84e02a4c5eeb6a693dd52f9e3aa8
Tim <tdp378@yahoo.com>
2026-01-24 22:29:02 -0500

    Ignore colcon build artifacts

--- install/jax_servo_interfacing/share/jax_servo_interfacing/launch/jax_servo.launch.py
+++ install/jax_servo_interfacing/share/jax_servo_interfacing/launch/jax_servo.launch.py
@@ -1,14 +1,1 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-
-
-def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='jax_servo_interfacing',
-            executable='jax_servo_node',
-            name='jax_servo_node',
-            output='screen'
-        )
-    ])
-
+/home/tdp378/Jax_Quadruped_ros2/build/jax_servo_interfacing/launch/jax_servo.launch.py
